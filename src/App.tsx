8eb2f65033bf7aa912962d402848a9b5ed7a844e
Brian Dombrowski <bdombro@gmail.com>
2021-07-01 14:38:57 -0500

    Added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-interface AppProps {}
+import React, { useEffect,useState } from 'react'
 
-function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+import logo from './logo.svg'
+
+export default function App() {
+	// Create the count state.
+	const [count, setCount] = useState(0)
+	// Create the counter (+1 every second).
+	useEffect(() => {
+		const timer = setTimeout(() => setCount(count + 1), 1000)
+		return () => clearTimeout(timer)
+	}, [count, setCount])
+	// Return the App component.
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
+				</p>
+				<p>
           Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+				</p>
+				<p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
             Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+					</a>
+				</p>
+			</header>
+		</div>
+	)
 }
-
-export default App;
